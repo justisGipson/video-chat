fa56223f0313b23daf31fe2ccfea0be95f6b4777
Justis Gipson <justis710@gmail.com>
2019-12-03 16:37:29 -0500

    stubbed out style tags, redis/index.js actions completed

--- server/redis/index.js
+++ server/redis/index.js
@@ -23,3 +23,68 @@ ChatRedis.prototype.addUser = (room, socketId, userObject) => {
         err => console.log(err)
     )
 }
+
+/**
+ * get all users by room
+ *  @param {} room
+ */
+ChatRedis.prototype.getUsers = (room) => {
+    return this.client.hgetallAsync(room).then(users => {
+        const userList = []
+        for (let user in users) {
+            userList.push(JSON.parse(users[user]))
+        }
+        return userList
+    }, err => {
+        console.log('getUsers', err)
+        return null
+    })
+}
+
+/**
+ * delete a user in room w/ socketId
+ * @param {} room
+ * @param {} socketId
+ */
+ChatRedis.prototype.delUser = (room, socketId) => {
+    return this.client.hdelAsync(room, socketId).then(
+        res => (res),
+        err => {
+            console.log('delUser', err)
+            return null
+        }
+    )
+}
+
+/**
+ * get user by room and socketId
+ * @param {} room
+ * @param {} socketId
+ */
+ChatRedis.prototype.getUser = (room, socketId) => {
+    return this.client.hgetAsync(room, socketId).then(
+        res => JSON.parse(res),
+        err => {
+            console.log('getUser', err)
+            return null
+        }
+    )
+}
+
+/**
+ * set user
+ * @param {} room
+ * @param {} socketId
+ * @param {} newValue
+ */
+ChatRedis.prototype.setUser = (room, socketId, newValue) => {
+    return this.client.hsetAsync(room, socketId, JSON.stringify(newValue)).then(
+        res => res,
+        err => {
+            console.log('setUser', err)
+            return null
+        }
+    )
+}
+
+module.exports = new ChatRedis()
--- src/components/ChatArea.vue
+++ src/components/ChatArea.vue
@@ -56,3 +56,7 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+
+</style>
